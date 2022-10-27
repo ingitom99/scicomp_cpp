02cb719b72446f3738e6a8580bb7ce3f6eafb4fe
Ingimar Tomasson <ingitom99@ingitom99-hpenvy.ingitom99@gmail.com>
2022-10-27 20:09:12 +0200

    init

---
+++ hello_world.cpp
@@ -0,0 +1,22 @@
+/*
+hello world in C++ !!!
+*/ 
+
+/*
+- "# include" is an instructive to import input output stream from the standard library.
+- "int" indicates that the last line of code within {} will return an integer value.
+- Inside () are arguments which can be specified by the user.
+- Code in {} is what we want the computer to execute.
+- "\n" creates a new line.
+- C++ ignores all white space; it is merely stylistic practice to indent the content of {}.
+- .cpp is just a choice, can use any extension
+- "std::cout" means look in the standard namespace for cout
+*/
+
+#include <iostream>
+
+int main(int argc, char* argv[])
+{
+    std::cout<<"Hello, world!\n";
+    return 0;
+}
---
+++ math_ops.cpp
@@ -0,0 +1,15 @@
+#include <cmath>
+#include <iostream>
+
+int main(int argc, char* argv[])
+{
+    double x = 1.0, y = 2.0, z;
+    z = x / y;     // division
+    z = x*y;       // multiplication
+    z = sqrt(x);   // square root
+    z = exp(y);    // exponential function
+    z = pow(x, y); // x to the power of y
+    z = M_PI;      // z stores the value of pi
+
+    return 0;
+}
